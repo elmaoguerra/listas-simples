dc58059c94d08d0a5a3c8bedebc67ca5c4b7daac
elmaoguerra <maoguerra007@gmail.com>
2015-04-27 10:06:26 -0500

    agregando mas funciones a perfiles_helper

--- fuentes/listasSimples/application/helpers/perfiles_helper.php
+++ fuentes/listasSimples/application/helpers/perfiles_helper.php
@@ -1,10 +1,6 @@
 <?php
 
 	function getMenuByPerfil($perfil){
-		
-		
-		
-		
 		if($perfil == PERFIL_SOLO_EJERCICIOS ){
 			
 			return "<ul class=\"menu\">
@@ -29,4 +25,57 @@
 		
 	}
 
-?>
+	function convertir_tiempo($tiempo_en_segundos)
+	{
+		$horas = floor($tiempo_en_segundos / 3600);
+		$minutos = floor(($tiempo_en_segundos - ($horas * 3600)) / 60);
+		$segundos = $tiempo_en_segundos - ($horas * 3600) - ($minutos * 60);
+	 
+		$hora_texto = "";
+		if ($horas > 0 ) {
+			$hora_texto .= $horas . "h ";
+		}
+	 
+		if ($minutos > 0 ) {
+			$hora_texto .= $minutos . "m ";
+		}
+	 
+		if ($segundos > 0 ) {
+			$hora_texto .= $segundos . "s";
+		}
+	 
+		return $hora_texto;
+	}
+
+	function get_ejer_intentos($resultados)
+	{
+		$intentos = 0;
+		$total_ejer = count($resultados);
+
+		$datos['intento_1'] = 0;
+		$datos['intento_2'] = 0;
+		$datos['intento_3'] = 0;
+		$datos['intento_4'] = 0;
+
+		foreach ($resultados as $res) {
+			$eficiencia = $res->eficiencia;
+			if ($eficiencia == 100) {
+				$intentos += 1;
+				$datos['intento_1']++;
+			}elseif ($eficiencia == 50) {
+				$intentos += 2;
+				$datos['intento_2']++;
+			}elseif ($eficiencia == 33) {
+				$intentos += 3;
+				$datos['intento_3']++;
+			}else{
+				$intentos += 3;
+				$datos['intento_4']++;
+			}
+		}
+		$datos['intentos'] = $intentos;
+		$datos['total_ejer'] = $total_ejer;
+		$datos['media'] = number_format(($intentos/$total_ejer), 2, ',', '');
+
+		return $datos;
+	}
